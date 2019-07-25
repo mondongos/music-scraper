479b53870f8ff99cf897b67132249b3030660636
Max Van Aken <mtvanaken@gmail.com>
2019-07-26 00:34:14 +0100

    Tests for creating bookmarks

--- spec/BorkListSpec.js
+++ spec/BorkListSpec.js
@@ -7,14 +7,32 @@ describe("Bork Lists", function() {
     bork = new Bork("facebook.com", "Facebook")
   });
 
-  it("should add a bork to the bork list", function() {
-    borkList.addNew(bork)
-    expect(borkList._borks.length).toEqual(1)
-  });
+  describe("when creating a bork", function() {
+    it("should add a bork to the bork list", function() {
+      borkList.addNew(bork)
+      expect(borkList._borks.length).toEqual(1)
+    });
 
-  it("should have a title", function() {
-    borkList.addNew(bork)
-    expect(borkList._borks[0].title).toEqual("Facebook")
+    it("should have a title", function() {
+      borkList.addNew(bork)
+      expect(borkList._borks[0].title).toEqual("Facebook")
+    })
+
+    it("should have a link", function() {
+      borkList.addNew(bork)
+      expect(borkList._borks[0].link).toEqual("facebook.com")
+    })
   })
 
+  describe("when reading a note", function() {
+    // to be completed
+  })
+
+  describe("when editing a note", function() {
+    // to be completed
+  })
+
+  describe("when deleting a note", function() {
+    // to be completed
+  })
 })
